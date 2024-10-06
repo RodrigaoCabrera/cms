3912d7d81be99d4233aaf891f7592655681e5770
Rodrigo Cabrera <rodrigo.cabrera@infracommerce.lat>
2024-10-06 13:51:17 -0300

    feat: dev-profile data structure

---
+++ src/api/dev-profile/controllers/dev-profile.ts
@@ -0,0 +1,7 @@
+/**
+ * dev-profile controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::dev-profile.dev-profile');
---
+++ src/api/dev-profile/routes/dev-profile.ts
@@ -0,0 +1,7 @@
+/**
+ * dev-profile router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::dev-profile.dev-profile');
